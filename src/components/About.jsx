579e61064eb4dfc724a6f65efb79f40e15de17df
Kalaga Dimple Sai Harikha <208568084+dsh-dev@users.noreply.github.com>
2026-08-11 09:27:43 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import profileImage from '../assets/projects/profile.png';
+import profileImage from '../assets/projects/profileImage.png';
 export default function About() {
   return (
     <section id="about" style={{
